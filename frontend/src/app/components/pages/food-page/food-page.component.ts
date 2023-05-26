8439c1386e8d3eeca1b22a94d9ab6e517c110c70
LtheBest <loicstonda@gmail.com>
2023-05-26 22:40:43 +0200

    end fronend

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/food';
 
@@ -12,7 +13,7 @@ export class FoodPageComponent implements OnInit {
 
   food!: Food;
 
-  constructor(activatedRoute:ActivatedRoute, foodService:FoodService) {
+  constructor(activatedRoute:ActivatedRoute, foodService:FoodService, private cartService:CartService, private router: Router) {
     activatedRoute.params.subscribe((params)=>{
       if(params.id)
       this.food = foodService.getFoodById(params.id);
@@ -22,4 +23,9 @@ export class FoodPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+
+  }
 }
--- frontend/src/app/shared/models/cartitem.ts
+++ frontend/src/app/shared/models/cartitem.ts
@@ -1,10 +1,11 @@
 import { Food } from "./food";
 
 export class CartItem{
-  constructor(food:Food){
-    this.food = food;
+  constructor(public food:Food){
+    
+    
   }
-  food!:Food;
+  
   quantity:number = 1;
   price:number= this.food.price;
 }
